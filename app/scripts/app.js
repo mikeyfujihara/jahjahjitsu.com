3f1538e384d67c2b746a1c24a69efde4fc7a9c1d
Mikey Fujihara <mikeyfujihara@gmail.com>
2013-04-02 14:32:21 -1000

    init site with yo angular --minsafe bp

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('jahjahjitsu.comApp', [])
+  .config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    $locationProvider.hashPrefix('#');
+    $locationProvider.html5Mode(true); //now there won't be a hashbang within URLs for browers that support HTML5 history
+  }]);
